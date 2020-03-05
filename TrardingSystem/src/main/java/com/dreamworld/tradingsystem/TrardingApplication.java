f0f264eb16c2b1d644dbe75e21f1bc56655dc82e
imrajeshgr <rajesh.gr9008@gmail.com>
2020-03-05 17:25:12 +0530

    init

---
+++ TrardingSystem/src/main/java/com/dreamworld/tradingsystem/TrardingApplication.java
@@ -0,0 +1,13 @@
+package com.dreamworld.tradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrardingApplication.class, args);
+    }
+
+}
